8b012d24f2232ace611ca8c04d45a7ca45ebfc1e
ClaytonDewey <clay@dryadmedia.com>
2026-04-27 08:11:14 -0500

    style: Start setting up homepage breakpoints

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,34 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { Button, Input } from './components';
+import { Icon } from './svg';
 
 export default function Home() {
   return (
     <div className='page-wrapper'>
       <header className='hero'>
-        <Image
-          src='/images/illustration-authentication.svg'
-          role='presentation'
-          alt=''
-          width={560}
-          height={920}
-        />
+        <Icon name='logo' />
+        <div className='hero-body'>
+          <h2>Keep track of your money and save for your future</h2>
+          <p>
+            Personal finance app puts you in control of your spending. Track
+            transactions, set budgets and add to savings pots easily.
+          </p>
+        </div>
       </header>
       <main>
         <div className='form-wrapper'>
+          <h1>Login</h1>
           <form className='form'>
-            <div>
-              <label htmlFor='email'>Email</label>
-              <Input type='email' id='email' name='email' />
-            </div>
-            <div>
-              <label htmlFor='password'>Password</label>
-              <Input type='password' id='password' name='password' />
-            </div>
+            <fieldset>
+              <div>
+                <label htmlFor='email'>Email</label>
+                <Input type='email' id='email' name='email' />
+              </div>
+              <div>
+                <label htmlFor='password'>Password</label>
+                <Input type='password' id='password' name='password' />
+              </div>
+            </fieldset>
             <Button type='submit' variant='primary'>
               Login
             </Button>
